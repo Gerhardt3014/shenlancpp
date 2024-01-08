3431fde64a03cc1be4cda3eb673162a9a6158251
hgh <15273177620@163.com>
2024-01-08 21:52:15 +0800

    first commit

---
+++ chapter3/cmake-build-debug/CMakeFiles/chapter3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chapter3.dir/main.cpp.o"
+  "CMakeFiles/chapter3.dir/main.cpp.o.d"
+  "chapter3"
+  "chapter3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chapter3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
